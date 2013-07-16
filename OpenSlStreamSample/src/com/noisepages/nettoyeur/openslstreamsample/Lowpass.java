226536b3ed6c7639c65aeabe12f8008651dced85
Peter Brinkmann <peter.brinkmann@gmail.com>
2013-07-15 20:16:33 -0400

    General reformatting.

--- OpenSlStreamSample/src/com/noisepages/nettoyeur/openslstreamsample/Lowpass.java
+++ OpenSlStreamSample/src/com/noisepages/nettoyeur/openslstreamsample/Lowpass.java
@@ -7,92 +7,97 @@ import java.io.IOException;
  */
 public class Lowpass {
 
-	static {
-		// Load the associated native library, which does most of the work.
-		System.loadLibrary("lowpass");
-	}
+  static {
+    // Load the associated native library, which does most of the work.
+    System.loadLibrary("lowpass");
+  }
 
-	private long streamPtr;  // Slightly naughty: We store a C pointer in a field of type long.
+  private long streamPtr; // Slightly naughty: We store a C pointer in a field of type long.
 
-	/**
-	 * Constructs a lowpass filter object for the given sample rate and buffer size.
-	 * 
-	 * @param sampleRate in Hertz.
-	 * @param bufferSize in frames.
-	 * @throws IOException if the sample rate or buffer size are not supported.
-	 */
-	public Lowpass(int sampleRate, int bufferSize) throws IOException {
-		streamPtr = open(sampleRate, bufferSize);
-		if (streamPtr == 0) {
-			throw new IOException("Unsupported audio parameters: " + sampleRate + ", " + bufferSize);
-		}
-	}
+  /**
+   * Constructs a lowpass filter object for the given sample rate and buffer size.
+   * 
+   * @param sampleRate in Hertz.
+   * @param bufferSize in frames.
+   * @throws IOException if the sample rate or buffer size are not supported.
+   */
+  public Lowpass(int sampleRate, int bufferSize) throws IOException {
+    streamPtr = open(sampleRate, bufferSize);
+    if (streamPtr == 0) {
+      throw new IOException("Unsupported audio parameters: " + sampleRate + ", " + bufferSize);
+    }
+  }
 
-	/**
-	 * Must be called before this object is garbage collected. Safe to call more than once.
-	 */
-	public void close() {
-		if (streamPtr != 0) {
-			close(streamPtr);
-			streamPtr = 0;
-		}
-	}
+  /**
+   * Must be called before this object is garbage collected. Safe to call more than once.
+   */
+  public void close() {
+    if (streamPtr != 0) {
+      close(streamPtr);
+      streamPtr = 0;
+    }
+  }
 
-	/**
-	 * Starts the OpenSL audio stream; will have no effect if the object has already been started.
-     * May not be called after close() has been called.
-	 * 
-	 * @throws IOException if the stream cannot be started.
-	 */
-	public void start() throws IOException {
-		if (streamPtr == 0) {
-			throw new IllegalStateException("Stream closed.");
-		}
-		if (start(streamPtr) != 0) {
-			throw new IOException("Unable to start OpenSL stream.");
-		}
-	}
+  /**
+   * Starts the OpenSL audio stream; will have no effect if the object has already been started. May
+   * not be called after close() has been called.
+   * 
+   * @throws IOException if the stream cannot be started.
+   */
+  public void start() throws IOException {
+    if (streamPtr == 0) {
+      throw new IllegalStateException("Stream closed.");
+    }
+    if (start(streamPtr) != 0) {
+      throw new IOException("Unable to start OpenSL stream.");
+    }
+  }
 
-	/**
-	 * Stops the OpenSL audio stream; will have no effect if the object has already been started.
-     * May not be called after close() has been called.
-	 */
-	public void stop() {
-		if (streamPtr == 0) {
-			throw new IllegalStateException("Stream closed.");
-		}
-		stop(streamPtr);
-	}
+  /**
+   * Stops the OpenSL audio stream; will have no effect if the object has already been started. May
+   * not be called after close() has been called.
+   */
+  public void stop() {
+    if (streamPtr == 0) {
+      throw new IllegalStateException("Stream closed.");
+    }
+    stop(streamPtr);
+  }
 
-	/**
-	 * Sets the smoothing factor alpha; safe to call while the stream is running.
-     * May not be called after close() has been called.
-	 * 
-	 * @param Smoothing factor in percent, 0 <= alpha <= 100.
-	 */
-	public void setAlpha(int alpha) {
-		if (streamPtr == 0) {
-			throw new IllegalStateException("Stream closed.");
-		}
-		setAlpha(streamPtr, alpha);
-	}
+  /**
+   * Sets the smoothing factor alpha; safe to call while the stream is running. May not be called
+   * after close() has been called.
+   * 
+   * @param Smoothing factor in percent, 0 <= alpha <= 100.
+   */
+  public void setAlpha(int alpha) {
+    if (streamPtr == 0) {
+      throw new IllegalStateException("Stream closed.");
+    }
+    setAlpha(streamPtr, alpha);
+  }
 
-	/**
-     * May not be called after close() has been called.
-     * 
-	 * @return true if the OpenSL audio stream filter is running.
-	 */
-	public boolean isRunning() {
-		if (streamPtr == 0) {
-			throw new IllegalStateException("Stream closed.");
-		}
-		return isRunning(streamPtr);
-	}
-	
-	private static native long open(int sampleRate, int bufferSize);
-	private static native void close(long streamPtr);
-	private static native int start(long streamPtr);
-	private static native void stop(long streamPtr);
-	private static native void setAlpha(long streamPtr, int alpha);
-	private static native boolean isRunning(long streamPtr);
+  /**
+   * May not be called after close() has been called.
+   * 
+   * @return true if the OpenSL audio stream filter is running.
+   */
+  public boolean isRunning() {
+    if (streamPtr == 0) {
+      throw new IllegalStateException("Stream closed.");
+    }
+    return isRunning(streamPtr);
+  }
+
+  private static native long open(int sampleRate, int bufferSize);
+
+  private static native void close(long streamPtr);
+
+  private static native int start(long streamPtr);
+
+  private static native void stop(long streamPtr);
+
+  private static native void setAlpha(long streamPtr, int alpha);
+
+  private static native boolean isRunning(long streamPtr);
 }
--- OpenSlStreamSample/src/com/noisepages/nettoyeur/openslstreamsample/MainActivity.java
+++ OpenSlStreamSample/src/com/noisepages/nettoyeur/openslstreamsample/MainActivity.java
@@ -12,77 +12,80 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.Switch;
 
 /**
- * Main activity for OpenSL sample app. This class is essentially boilerplate;
- * most of the interesting bits are in {@link OpenSlParams} and {@link Lowpass}.
+ * Main activity for OpenSL sample app. This class is essentially boilerplate; most of the
+ * interesting bits are in {@link OpenSlParams} and {@link Lowpass}.
  */
-public class MainActivity extends Activity implements OnCheckedChangeListener, OnSeekBarChangeListener {
+public class MainActivity extends Activity
+    implements
+      OnCheckedChangeListener,
+      OnSeekBarChangeListener {
 
-	private Lowpass lowpass;
-	private SeekBar filterBar;
-	private Switch playSwitch;
+  private Lowpass lowpass;
+  private SeekBar filterBar;
+  private Switch playSwitch;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		filterBar = (SeekBar) findViewById(R.id.filterBar);
-		filterBar.setOnSeekBarChangeListener(this);
-		playSwitch = (Switch) findViewById(R.id.playSwitch);
-		playSwitch.setOnCheckedChangeListener(this);
-	}
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    filterBar = (SeekBar) findViewById(R.id.filterBar);
+    filterBar.setOnSeekBarChangeListener(this);
+    playSwitch = (Switch) findViewById(R.id.playSwitch);
+    playSwitch.setOnCheckedChangeListener(this);
+  }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-		try {
-			OpenSlParams params = OpenSlParams.createInstance(this);
-			lowpass = new Lowpass(params.getSampleRate(), params.getBufferSize());
-			lowpass.setAlpha(filterBar.getProgress());
-			if (playSwitch.isChecked()) {
-				lowpass.start();
-			}
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+  @Override
+  protected void onStart() {
+    super.onStart();
+    try {
+      OpenSlParams params = OpenSlParams.createInstance(this);
+      lowpass = new Lowpass(params.getSampleRate(), params.getBufferSize());
+      lowpass.setAlpha(filterBar.getProgress());
+      if (playSwitch.isChecked()) {
+        lowpass.start();
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
-	@Override
-	protected void onStop() {
-		super.onStop();
-		lowpass.close();
-	}
+  @Override
+  protected void onStop() {
+    super.onStop();
+    lowpass.close();
+  }
 
-	@Override
-	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		if (isChecked) {
-			try {
-				lowpass.start();
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-		} else {
-			lowpass.stop();
-		}
-	}
+  @Override
+  public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+    if (isChecked) {
+      try {
+        lowpass.start();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    } else {
+      lowpass.stop();
+    }
+  }
 
-	@Override
-	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-		lowpass.setAlpha(progress);
-	}
+  @Override
+  public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+    lowpass.setAlpha(progress);
+  }
 
-	@Override
-	public void onStartTrackingTouch(SeekBar seekBar) {
-		// Do nothing.
-	}
+  @Override
+  public void onStartTrackingTouch(SeekBar seekBar) {
+    // Do nothing.
+  }
 
-	@Override
-	public void onStopTrackingTouch(SeekBar seekBar) {
-		// Do nothing.
-	}
+  @Override
+  public void onStopTrackingTouch(SeekBar seekBar) {
+    // Do nothing.
+  }
 }
